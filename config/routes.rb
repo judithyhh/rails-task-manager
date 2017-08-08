49e7570bacc97ef2148d5232f605fd503094771f
Judith Hsiang <judithyhh@gmail.com>
2017-08-08 13:46:29 +0800

    Added routes content.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
 
-  # read all tasks
+  # Read all tasks
+  get "tasks", to: "tasks#index"
 
-  # read one task
+  # Read one task
+  get "tasks/:id", to: "tasks#show"
 
-  # creat a task (2 requests)
+  # Create a task (2 requests)
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
 
+  # Update a task (2 requests)
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
 
+  # Delete a task
+  delete "tasks/:id", to: "tasks#destroy"
 
 end
