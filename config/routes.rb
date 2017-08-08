3f586786da1b2010c958058296e87158e23edd94
Judith Hsiang <judithyhh@gmail.com>
2017-08-08 16:24:21 +0800

    CRUD completed.

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
 
-  # Read all tasks
-  get "tasks", to: "tasks#index"
+  # # Read all tasks
+  # get "tasks", to: "tasks#index"
 
-  # Read one task
-  get "tasks/:id", to: "tasks#show"
+  # # Read one task
+  # get "tasks/:id", to: "tasks#show"
 
-  # Create a task (2 requests)
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
+  # # Create a task (2 requests)
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
 
-  # Update a task (2 requests)
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
+  # # Update a task (2 requests)
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
 
-  # Delete a task
-  delete "tasks/:id", to: "tasks#destroy"
+  # # Delete a task
+  # delete "tasks/:id", to: "tasks#destroy"
 
 end
