bcb4f86f1a7b738a43012eda694710de4fcfce22
Judith Hsiang <judithyhh@gmail.com>
2017-08-08 13:31:09 +0800

    Added tasks controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # read all tasks
+
+  # read one task
+
+  # creat a task (2 requests)
+
+
+
 end
